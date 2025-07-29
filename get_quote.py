b9ab65f5840760c217ccbdba6a0bb921828c32a3
Anjatiana <131655099+Anjatiana-EternalStudent@users.noreply.github.com>
2025-07-29 18:36:49 +0300

    Update get_quote.py

--- get_quote.py
+++ get_quote.py
@@ -5,7 +5,7 @@ def get_random_quote():
         response = requests.get("https://zenquotes.io/api/random/")
         response.raise_for_status()
         for quote in response.json():
-            return f"\"{quote['q']}\" \n- {quote['a']}"
+            return f"\"{quote['q']}\" \n- {quote['a']} -"
     except requests.exceptions.HTTPError as e:
         print("Erreur http:", e)
         return "Citation indisponible pour aujourdhui"
